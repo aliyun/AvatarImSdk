9062f2c7748acdd604cdb52fe183700a8f1d05f5
guling.hhh <huhonghao.hhh@alibaba-inc.com>
2022-07-21 16:07:37 +0800

    完善打包逻辑

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 // import commonjs from 'rollup-plugin-commonjs';
 
-// import pkg from './package.json';
+import pkg from './package.json';
 
 const config = [
   {
